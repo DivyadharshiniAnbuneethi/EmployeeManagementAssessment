d75dc90acc710bb18d7924093db6ecf1a2d9091a
DivyadharshiniAnbuneethi <96973374+DivyadharshiniAnbuneethi@users.noreply.github.com>
2022-01-16 14:59:07 +0530

    employee
    docker jenkins

---
+++ src/main/java/com/example/demo/EmployeeManagementAssessmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementAssessmentApplication.class, args);
+		System.out.println("Execution Starts");
+	}
+
+}
